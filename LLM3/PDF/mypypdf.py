e03a4fcef08b63f68747aef1d1c906a1146fabea
pia222sk20 <pia222.sk20@gmail.com>
2025-12-02 12:11:24 +0900

    .

--- LLM3/PDF/mypypdf.py
+++ LLM3/PDF/mypypdf.py
@@ -2,3 +2,8 @@ from langchain_community.document_loaders import PyPDFLoader
 
 loader = PyPDFLoader("document.pdf")
 documents = loader.load()  # 페이지별 Document 리스트
+for doc in documents:
+    print(f"소스 : {doc.metadata['source']}")
+    print(f"페이지 : {doc.metadata['page']}")
+    print(f"컨텐츠 길이 : {len(doc.page_content)}문자")
+    print(f"컨텐츠 미리보기 : {doc.page_content[:200]}...")
