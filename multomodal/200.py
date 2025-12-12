9e1a1e173ef40df67d7d37e44803393ec9fcaa6c
pia222sk20 <pia222.sk20@gmail.com>
2025-12-12 14:01:46 +0900

    .

--- multomodal/200.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("food101", split="train")
-label_names = dataset.features["label"].names
-
-for cls in ['apple_pie','baby_back_ribs','baklava','beef_carpaccio','beef_tartare']:
-    print(cls, "→", label_names.index(cls))
