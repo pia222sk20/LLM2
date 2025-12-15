4f8c653a4f7899ec26142e25127157d967b99d35
pia222sk20 <pia222.sk20@gmail.com>
2025-12-15 14:30:01 +0900

    .

--- multomodal/1.basic/02.vit.이미지전처리.py
+++ multomodal/1.basic/02.vit.이미지전처리.py
@@ -149,7 +149,7 @@ def visualize_preprocess(img, img_tensor, augmented_images):
     patch_size = 16
     img_np = img_display.permute(1,2,0).numpy()
     axes[0,2].imshow(img_np)
-    # 그리드 그리기
+    # 그리드 그리기 - 실제 패치로 임베딩한 공간정보가 아니라 .vit가 이미지를 공간단위로 나누어 본다
     for i in range(0,224,patch_size):
         axes[0,2].axhline(y=i, color='red',linewidth=0.5)
         axes[0,2].axvline(y=i, color='red',linewidth=0.5)
