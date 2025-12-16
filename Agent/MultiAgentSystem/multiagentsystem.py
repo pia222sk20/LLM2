3eef4a86d22f7fc90add2720926c26f5086b61d9
pia222sk20 <pia222.sk20@gmail.com>
2025-12-16 15:27:16 +0900

    .

--- Agent/MultiAgentSystem/multiagentsystem.py
+++ Agent/MultiAgentSystem/multiagentsystem.py
@@ -97,3 +97,60 @@ class VectorDBAgent(SpecializedAgent):
         except Exception as e:
             print(f'임베딩 생성 실패 : {e}')
             return [0.0]*1536
+    def _initialize_db(self):
+        '''영화 데이터를 vectordb 저장'''
+        for movie in SAMPLE_MOVIES:
+            doc_text = f"{movie['title']} ({movie['year']}). {movie['plot']}"
+
+            self.collection.add(
+                ids=[movie['id']],
+                documents=[doc_text],
+                embeddings=[self._get_embedding(doc_text)],
+                metadatas=[{
+                    'title':movie['title'],
+                    'year':movie['year'],
+                    'director':movie['director'],
+                    'rating':movie['rating']
+                }]
+            )
+    def _handle_message(self, message:Message)->Dict[str, Any]:
+        content = message.content
+        query = content.get('query','')
+        top_k = content.get('top_k',3)
+        query_embedding = self._get_embedding(query)
+        results =  self.collection.query(
+            query_embeddings=[query_embedding],
+            n_results=top_k
+        )
+        retrieved_docs = []
+        if results['documents'] and len(results['documents']) > 0:
+            for i, doc in enumerate(results['documents'][0]):
+                metadata = results['metadatas'][0][i]
+                distance = results['distances'][0][i] if 'distances' in results else 0
+
+                retrieved_docs.append({
+                    'id' : results['ids'][0][i],
+                    'content':doc,
+                    'metadata' : metadata,
+                    'similarity' : 1-distance
+                })
+        return {
+            'status':'retrieved',
+            'query' : query,
+            'results':retrieved_docs,
+            'count' : len(retrieved_docs)
+        }
+# pip install networkx
+import networkx as nx
+class KnowledgeGraphAgent(SpecializedAgent):
+    '''지식그래프 검색 에이전트(Networkx)'''
+    def __init__(self, name:str):
+        super().__init__(name, 'knowledge_graph')
+        self.graph = nx.DiGraph()
+        self._initialize_graph()
+    def _initialize_graph(self):
+        '''영화 관계 그래프 생성'''
+        for movie in SAMPLE_MOVIES:
+            # 영화 노드
+            self.graph.add_node(movie['id'],type='movie',title=movie['title'],year=movie['year'])
+            
