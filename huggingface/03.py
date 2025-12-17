5c056b0a539ae56045f52619d95f28deb36cfdf3
pia222sk20 <pia222.sk20@gmail.com>
2025-12-17 09:42:53 +0900

    .

--- huggingface/03.py
+++ huggingface/03.py
@@ -7,7 +7,13 @@
     # login(token='hf-token')
 
 import torch
+import os
 from diffusers import FluxPipeline
+from huggingface_hub import login
+from dotenv import load_dotenv
+load_dotenv()
+
+login(token=os.getenv('HF_TOKEN'))
 
 pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
